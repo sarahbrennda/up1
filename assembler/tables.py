ee7013990a804f98b36999383ec0da00d6d8391c
vlab <vlab@vlab.dc.ufscar.br>
2021-06-02 17:50:54 -0300

    foo

--- assembler/tables.py
+++ assembler/tables.py
@@ -8,11 +8,11 @@ inst_table = {
     "JNZ"   : "1"
 }
 
-space_table = {
-    "code"   : 128,
-    "video"  :  80,
-    "unused" :  32, 
-    "data"   :  16
+space_table = {       #   dec   |        bin        |  hex
+    "code"   : 128,   #   0:127 | 00000000:01111111 | 00:7f
+    "video"  :  80,   # 128:207 | 10000000:11001111 | 80:cf
+    "unused" :  32,   # 208:239 | 11010000:11101111 | d0:ef
+    "data"   :  16    # 240:255 | 11110000:11111111 | f0:ff
 }
 
 fields_table = {
